a457454b4d87fb607ec5b269e143f212dbddc730
Benjamin Wong <chiyeonwastaken@gmail.com>
2024-02-23 13:24:27 -0800

    added express to server for requests

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const { log } = require("./utils.js")
-const fb = require("./firebase.js") 
+const fb = require("./firebase.js")
 
 const PORT = process.env.port | 3000
 
-const server = require("http").createServer((req, res) => {
+const app = require("express")()
+const server = require("http").createServer(app)
+const cors = require("cors")
+const body_parser = require("body-parser")
+
+app.use(cors({ origin: "*" })) // accept from all origins for now, on prod change to specific URLs!
+app.use(body_parser.json())
+
+// set up req here
+
+app.get("/", (req, res) => {
    res.writeHead(200, { "Content-Type": "text/html" })
 
    res.write("<pre>In Development</pre>");
@@ -11,8 +21,8 @@ const server = require("http").createServer((req, res) => {
 })
 
 const start = () => {
-   server.listen(PORT, () => {
-      log("Server has started.")
+   server.listen(PORT, async () => {
+      log("Server has started on port " + PORT)
    })
 }
 
