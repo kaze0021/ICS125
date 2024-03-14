1c9b90e9a1be817860360dcba9a71f0af71ec0ba
Benjamin Wong <chiyeonwastaken@gmail.com>
2024-03-13 18:27:34 -0700

    users can set up basic profiles, allowed to change data, better error handling

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ const body_parser = require("body-parser")
 
 const fb = require("./firebase.js")
 
+const isNumber = (n) => {
+   return typeof n === 'number'
+}
+
 const setup_user_sessions = () => {
    // firebase.delete_doc_path("sessions/users")
    // fb.set_doc_path("sessions/users", {})
@@ -33,8 +37,17 @@ const new_user_session = async (token, uid) => {
 
 // given a session access token, returns the UID that corresponds to it, or -1 if no session exists
 const get_user_id = async (token) => {
-   let data = await fb.get_doc("sessions", token)
-   return data ? data.uid : -1
+   try {
+      let data = await fb.get_doc("sessions", token)
+      return data ? data.uid : -1
+   } catch (e) {
+      return -1
+   }
+}
+
+// gets age of user using their stored birthday
+const get_user_data = async (uid) => {
+   return await fb.get_doc("users", uid)
 }
 
 /**
@@ -97,59 +110,146 @@ const signup = async (req, res) => {
    }
 }
 
+// sets the basic (mostly unchanging) data of our users
+// all should be strings
+// birthday shoudl be in ISO format: YYYY-MM-DD
+// height is in feet
+// weight is in lbs
+// example of a 'proper' input:
+//
+// set_user_data("2002-02-18", "Male", 1.1, 1000)
+const set_user_data = async (req, res) => {
+   try {
+      // check data
+      let data = {
+         birthday: req.body.birthday,
+         gender: req.body.gender,
+         height: parseFloat(req.body.height),
+         weight: parseFloat(req.body.weight)
+      }
+      let uid = await get_user_id(req.body.token)
+
+      // adapted from https://www.30secondsofcode.org/js/s/iso-format-date-with-timezone/
+      const is_ISO_date = (v) => {
+         const d = new Date(v);
+         return !Number.isNaN(d.valueOf()) && d.toISOString().includes(v);
+      };
+
+      // todo move this to config class
+      let valid_genders = [
+         "Male", "Female", "Non-Binary"
+      ]
+
+      let keys = Object.keys(data)
+      if (!keys.includes("birthday") ||
+         !keys.includes("height") ||
+         !keys.includes("weight") ||
+         !keys.includes("gender")) {
+         return res.status(400).json({ message: "Error: Invalid user data" })
+      }
+
+      // ensure is "valid" data
+      if (!isNumber(data.height) || data.height < 0 || data.height > 10) {
+         return res.status(400).json({ message: "Invalid height" })
+      } else if (!isNumber(data.weight) || data.weight < 0) {
+         return res.status(400).json({ message: "Invalid weight" })
+      } else if (!valid_genders.includes(data.gender)) {
+         return res.status(400).json({ message: "Invalid gender" })
+      } else if (!is_ISO_date(data.birthday)) {
+         return res.status(400).json({ message: "Invalid birthday" })
+      }
+
+      await fb.update_doc("users", uid, {
+         userData: data
+      })
+
+      res.status(200).json({ message: "Successfully updated user data" })
+   } catch (e) {
+      log("Error: " + e)
+      res.status(400).json({ message: "Invalid or malformed request" })
+   }
+}
+
 // given a session token & the updated intake in oz, updates in users db
 // returns a status code, -1 for error and 1 for successful update
 const update_user_water = async (req, res) => {
-   let token = req.body.token;
-   let intake = req.body.data;
+   try {
+      let token = req.body.token;
+      let intake = parseFloat(req.body.data);
 
-   let uid = await get_user_id(token)
+      if (isNaN(intake)) return res.status(400).json({ message: "Invalid amount" })
 
-   if (uid == -1) {
-      res.status(400).json({ message: "Invalid user session. Try logging in again." })
-      return
-   }
+      let uid = await get_user_id(token)
 
-   if (await users.updateWaterIntake(uid, intake) == -1) {
-      res.status(400).json({ message: "Server error: Couldn't update water intake." })
-   } else {
-      res.status(200).json({ message: "Water intake successfully updated!" })
+      // error catching/handling
+      if (uid == -1) {
+         return res.status(400).json({ message: "Invalid user session. Try logging in again." })
+      } else if (!isNumber(intake) || intake < 0) {
+         return res.status(400).json({ message: "Invalid water amount" })
+      }
+
+      if (await users.updateWaterIntake(uid, intake) == -1) {
+         res.status(400).json({ message: "Server error: Couldn't update water intake." })
+      } else {
+         res.status(200).json({ message: "Water intake successfully updated!" })
+      }
+   } catch(e) {
+      log("Error: " + e)
+      res.status(400).json({ message: "Invalid or malformed request" })
    }
 }
 
 const update_user_sleep = async (req, res) => {
-   let token = req.body.token;
-   let hours = req.body.data;
+   try {
+      let token = req.body.token;
+      let hours = parseFloat(req.body.data);
 
-   let uid = await get_user_id(token)
+      if (isNaN(hours)) return res.status(400).json({ message: "Invalid amount" })
 
-   if (uid == -1) {
-      res.status(400).json({ message: "Invalid user session. Try logging in again." })
-      return
-   }
+      let uid = await get_user_id(token)
 
-   if (await users.updateSleepHours(uid, hours) == -1) {
-      res.status(400).json({ message: "Server error: Couldn't update sleep amount." })
-   } else {
-      res.status(200).json({ message: "Sleep amount successfully updated!" })
+      // error catching/handling
+      if (uid == -1) {
+         return res.status(400).json({ message: "Invalid user session. Try logging in again." })
+      } else if (!isNumber(hours) || hours < 0) {
+         return res.status(400).json({ message: "Invalid sleep amount" })
+      }
+
+      if (await users.updateSleepHours(uid, hours) == -1) {
+         res.status(400).json({ message: "Server error: Couldn't update sleep amount." })
+      } else {
+         res.status(200).json({ message: "Sleep amount successfully updated!" })
+      }
+   } catch(e) {
+      log("Error: " + e)
+      res.status(400).json({ message: "Invalid or malformed request" })
    }
 }
 
 const update_user_exercise = async (req, res) => {
-   let token = req.body.token;
-   let hours = req.body.data;
+   try {
+      let token = req.body.token;
+      let hours = parseFloat(req.body.data);
 
-   let uid = await get_user_id(token)
+      if (isNaN(hours)) return res.status(400).json({ message: "Invalid amount" })
+      
+      let uid = await get_user_id(token)
 
-   if (uid == -1) {
-      res.status(400).json({ message: "Invalid user session. Try logging in again." })
-      return
-   }
+      // error catching/handling
+      if (uid == -1) {
+         return res.status(400).json({ message: "Invalid user session. Try logging in again." })
+      } else if (!isNumber(hours) || hours < 0) {
+         return res.status(400).json({ message: "Invalid exercise amount" })
+      }
 
-   if (await users.updateExerciseHours(uid, hours) == -1) {
-      res.status(400).json({ message: "Server error: Couldn't update exercise amount." })
-   } else {
-      res.status(200).json({ message: "Exercise amount successfully updated!" })
+      if (await users.updateExerciseHours(uid, hours) == -1) {
+         res.status(400).json({ message: "Server error: Couldn't update exercise amount." })
+      } else {
+         res.status(200).json({ message: "Exercise amount successfully updated!" })
+      }
+   } catch(e) {
+      log("Error: " + e)
+      res.status(400).json({ message: "Invalid or malformed request" })
    }
 }
 
@@ -159,6 +259,7 @@ app.use(body_parser.json())
 // set up req here
 app.post("/login", login)
 app.post("/signup", signup)
+app.post("/set_user_data", set_user_data)
 app.post("/update_water", update_user_water)
 app.post("/update_exercise", update_user_exercise)
 app.post("/update_sleep", update_user_sleep)
