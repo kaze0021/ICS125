e0c20b69247d650e0a14238b05ff26ea49a82080
Grace Zhen <110500301+G-Zhen@users.noreply.github.com>
2024-03-15 19:46:10 -0700

    get rid of generationConfig

--- backend/geminiAdvice.js
+++ backend/geminiAdvice.js
@@ -3,10 +3,10 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require("dotenv");
 dotenv.config();
 
-async function generateText(prompt, generationConfig = {}) {
+async function generateText(prompt) {
   const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-  const result = await model.generateContentStream([prompt], generationConfig);
+  const result = await model.generateContentStream([prompt]);
   let generatedText = "";
 
   for await (const chunk of result.stream) {
